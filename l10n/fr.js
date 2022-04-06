220b7227122501b4e3bf2111b6a17832cbe03218
ownClouders <devops@owncloud.com>
2022-04-06 00:40:09 +0000

    [tx] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -66,4 +66,4 @@ OC.L10N.register(
     "Enabled" : "Activé",
     "Disabled" : "Désactivé"
 },
-"nplurals=3; plural=(n==0 || n==1) ? 0 : n != 0 && n % 1000000 == 0 ? 1 : 2;");
+"nplurals=2; plural=(n > 1);");
